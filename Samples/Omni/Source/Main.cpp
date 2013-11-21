c9f190a571e597c486dbf19148f53c1dbeb3aefa
Malinin Alexander <Malinin Alexander@localhost>
2013-11-21 21:55:57 +0400

    Misc.

--- Samples/Omni/Source/Main.cpp
+++ Samples/Omni/Source/Main.cpp
@@ -17,6 +17,8 @@ inline void Sleep(int ms) {usleep(ms*1000);}
 
 TERMINAL_TAKE_CARE_OF_WINMAIN
 
+void TestWGL4();
+
 int main()
 {
 	terminal_open();
@@ -109,7 +111,9 @@ int main()
 	terminal_refresh();
 	terminal_read();
 
-	/*
+	TestWGL4();
+
+	//*
 	terminal_close();
 	return 0;
 	//*/
@@ -142,3 +146,268 @@ int main()
 	terminal_close();
 	return 0;
 }
+
+// U+0020..U+007F: C0 Controls and Basic Latin
+// U+00A0..U+00FF: C1 Controls and Latin-1 Supplement
+// U+0100..U+017F: Latin Extended-A
+// U+0190..U+019F: Latin Extended-B
+// U+01F0..U+01FF: --||--
+// U+02C0..U+02CF: Spacing Modifier Letters
+// U+02D0..U+02DF: --||--
+// U+0370..U+03CF: Greek
+// U+0400..U+045F: Cyrillic
+// U+0490..U+049F: --||--
+// U+1E80..U+1E8F: Latin Extended Additional
+// U+1EF0..U+1EFF: --||--
+// U+2010..U+204F: General Punctuation
+// U+2070..U+207F: Super/Subscripts
+// U+20A0..U+20AF: Currency Symbols
+// U+2100..U+212F: Letterlike symbols
+// U+2150..U+215F: Number Forms
+// U+2190..U+219F: Arrows
+// U+21A0..U+21AF: --||--
+// U+2200..U+222F: Mathematical Operators
+// U+2240..U+224F: --||--
+// U+2260..U+226F: --||--
+// U+2300..U+232F: Miscellaneous Technical
+// U+2500..U+257F: Box drawing characters
+// U+2580..U+259F: Block Elements
+// U+25A0..U+25EF: Geometric Shapes
+// U+2630..U+264F: Miscellaneous Symbols
+// U+2660..U+266F: --||--
+// U+F000..U+F00F: Private Use Area
+// U+FB00..U+FB00: Alphabetic Presentation Forms
+
+
+#include <vector>
+#include <string>
+#include <cmath>
+#include <set>
+#include <iostream>
+
+namespace
+{
+	void AddSubrange(std::vector<std::pair<int, int> >& result, int start, int end)
+	{
+		result.push_back(std::pair<int, int>(std::floor(start/16.0)*16, std::ceil(end/16.0)*16-1));
+		std::cout << "  " << start << "-" << end << " -> " << result.back().first << "-" << result.back().second << "\n";
+	}
+
+	std::vector<std::pair<int, int> > SplitRange(std::set<int>& codes)
+	{
+		std::cout << "R:\n";
+
+		std::vector<std::pair<int, int> > result;
+
+		int start = 0, end = 0;
+		for (std::set<int>::iterator i=codes.begin(); i!=codes.end(); i++)
+		{
+			int code = *i;
+			if (start == 0) start = code;
+			if (end == 0) end = code;
+			if (code-end > 16)
+			{
+				AddSubrange(result, start, code);
+				start = code;
+			}
+			end = code;
+		}
+		if (start > 0) AddSubrange(result, start, end);
+
+		return result;
+	}
+
+	struct UnicodeRange
+	{
+		std::string name;
+		std::set<int> codes;
+
+		UnicodeRange(const std::string& name):
+			name(name)
+		{ }
+
+		void Add(int code)
+		{
+			codes.insert(code);
+		}
+
+		void Add(int from, int to)
+		{
+			for (int i=from; i<=to; i++) codes.insert(i);
+		}
+	};
+
+	std::vector<UnicodeRange> FillRanges()
+	{
+		std::vector<UnicodeRange> ranges;
+		ranges.push_back(UnicodeRange("C0 Controls and Basic Latin"));
+		ranges.back().Add(0x0020, 0x007F);
+		ranges.push_back(UnicodeRange("C1 Controls and Latin-1 Supplement"));
+		ranges.back().Add(0x00A0, 0x00FF);
+		ranges.push_back(UnicodeRange("Latin Extended-A"));
+		ranges.back().Add(0x0100, 0x017F);
+		ranges.push_back(UnicodeRange("Latin Extended-B"));
+		ranges.back().Add(0x0192);
+		ranges.back().Add(0x01FA, 0x01FF);
+		ranges.push_back(UnicodeRange("Spacing Modifier Letters"));
+		ranges.back().Add(0x02C6);
+		ranges.back().Add(0x02C7);
+		ranges.back().Add(0x02C9);
+		ranges.back().Add(0x02D8, 0x02DD);
+		ranges.push_back(UnicodeRange("Greek"));
+		ranges.back().Add(0x037E);
+		ranges.back().Add(0x0384, 0x038A);
+		ranges.back().Add(0x038C);
+		ranges.back().Add(0x038E, 0x03A1);
+		ranges.back().Add(0x03A3, 0x03CE);
+		ranges.push_back(UnicodeRange("Cyrillic"));
+		ranges.back().Add(0x0400, 0x045F);
+		ranges.back().Add(0x0490, 0x0491);
+		ranges.push_back(UnicodeRange("Latin Extended Additional"));
+		ranges.back().Add(0x1E80, 0x1E85);
+		ranges.back().Add(0x1EF2, 0x1EF3);
+		ranges.push_back(UnicodeRange("General Punctuation"));
+		ranges.back().Add(0x2013, 0x2015);
+		ranges.back().Add(0x2017, 0x201E);
+		ranges.back().Add(0x2020, 0x2022);
+		ranges.back().Add(0x2026);
+		ranges.back().Add(0x2030);
+		ranges.back().Add(0x2032, 0x2033);
+		ranges.back().Add(0x2039, 0x203A);
+		ranges.back().Add(0x203C);
+		ranges.back().Add(0x203E);
+		ranges.back().Add(0x2044);
+		ranges.push_back(UnicodeRange("Super/subscript"));
+		ranges.back().Add(0x207F);
+		ranges.push_back(UnicodeRange("Currency symbols"));
+		ranges.back().Add(0x20A3, 0x20A4);
+		ranges.back().Add(0x20A7);
+		ranges.back().Add(0x20AC);
+		ranges.push_back(UnicodeRange("Letterlike symbols"));
+		ranges.back().Add(0x2105);
+		ranges.back().Add(0x2113);
+		ranges.back().Add(0x2116);
+		ranges.back().Add(0x2122);
+		ranges.back().Add(0x2126);
+		ranges.back().Add(0x212E);
+		ranges.push_back(UnicodeRange("Number forms"));
+		ranges.back().Add(0x215B, 0x215E);
+		ranges.push_back(UnicodeRange("Arrows"));
+		ranges.back().Add(0x2190, 0x2195);
+		ranges.back().Add(0x21A8);
+		ranges.push_back(UnicodeRange("Mathematical operators"));
+		ranges.back().Add(0x2202);
+		ranges.back().Add(0x2206);
+		ranges.back().Add(0x220F);
+		ranges.back().Add(0x2211, 0x2212);
+		ranges.back().Add(0x2215);
+		ranges.back().Add(0x2219, 0x221A);
+		ranges.back().Add(0x221E, 0x221F);
+		ranges.back().Add(0x2229);
+		ranges.back().Add(0x222B);
+		ranges.back().Add(0x2248);
+		ranges.back().Add(0x2260, 0x2261);
+		ranges.back().Add(0x2264, 0x2265);
+		ranges.push_back(UnicodeRange("Miscellaneous technical"));
+		ranges.back().Add(0x2302);
+		ranges.back().Add(0x2310);
+		ranges.back().Add(0x2320, 0x2321);
+		ranges.push_back(UnicodeRange("Box drawing symbols"));
+		ranges.back().Add(0x2500, 0x257F);
+		ranges.push_back(UnicodeRange("Block elements"));
+		ranges.back().Add(0x2580, 0x259F);
+		ranges.push_back(UnicodeRange("Geometric shapes"));
+		ranges.back().Add(0x25A0, 0x25A1);
+		ranges.back().Add(0x25AA, 0x25AC);
+		ranges.back().Add(0x25B2);
+		ranges.back().Add(0x25BA);
+		ranges.back().Add(0x25BC);
+		ranges.back().Add(0x25C4);
+		ranges.back().Add(0x25CA, 0x25CB);
+		ranges.back().Add(0x25CF);
+		ranges.back().Add(0x25D8, 0x25D9);
+		ranges.back().Add(0x25E6);
+		ranges.push_back(UnicodeRange("Miscellaneous shapes"));
+		ranges.back().Add(0x263A, 0x263C);
+		ranges.back().Add(0x2640);
+		ranges.back().Add(0x2642);
+		ranges.back().Add(0x2660);
+		ranges.back().Add(0x2663);
+		ranges.back().Add(0x2665, 0x2666);
+		ranges.back().Add(0x266A, 0x266B);
+		ranges.push_back(UnicodeRange("Private Use Area"));
+		ranges.back().Add(0xF001, 0xF002);
+		ranges.push_back(UnicodeRange("Alphabet presentation form"));
+		ranges.back().Add(0xFB01, 0xFB02);
+		return ranges;
+	}
+}
+
+void TestWGL4()
+{
+	terminal_set("window: size=80x25, cellsize=auto, title='WGL4'; font=default");
+
+	std::vector<UnicodeRange> ranges = FillRanges();
+
+	const int hoffset = 40;
+	int current_range = 0;
+
+	while (true)
+	{
+		terminal_clear();
+		terminal_wprint(1, 1, L"[color=darker green]Use ↑/↓ keys to select range:");
+
+		for (int i=0; i<ranges.size(); i++)
+		{
+			bool selected = i == current_range;
+			terminal_color(selected? color_from_name("orange"): color_from_name("light gray"));
+			terminal_printf(1, 2+i, "[U+2219] %s", ranges[i].name.c_str());
+		}
+
+		UnicodeRange& range = ranges[current_range];
+
+		std::vector<std::pair<int, int> > subranges = SplitRange(range.codes);
+		for (int i=0, y=0; i<subranges.size(); i++)
+		{
+			std::pair<int, int>& subrange = subranges[i];
+			for (int j=0; j<16; j++)
+			{
+				terminal_printf(hoffset+6+j*2, 1, "[color=orange]%X", j);
+			}
+			for (int code=subrange.first; code<=subrange.second; code++)
+			{
+				if (code%16 == 0) terminal_printf(hoffset, 3+y*2, "[color=orange]%04X:", code);
+
+				bool included = range.codes.count(code);
+				terminal_color(included? color_from_name("white"): color_from_name("dark gray"));
+				terminal_put(hoffset+6+(code%16)*2, 3+y*2, code);
+
+				if ((code+1)%16 == 0) y += 1;
+			}
+		}
+		terminal_refresh();
+
+		bool exit = false;
+		do
+		{
+			int key = terminal_read();
+			if (key == TK_ESCAPE)
+			{
+				exit = true;
+				break;
+			}
+			else if (key == TK_UP)
+			{
+				if (current_range > 0) current_range -= 1;
+			}
+			else if (key == TK_DOWN)
+			{
+				if (current_range < ranges.size()-1) current_range += 1;
+			}
+		}
+		while (terminal_has_input());
+
+		if (exit) break;
+	}
+}
+
