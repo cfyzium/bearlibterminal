5e9cc01e86dd9ece3e14a84c947d0a1a77c7e13c
Malinin Alexander <Malinin Alexander@localhost>
2014-01-14 18:30:52 +0400

    C# and Pascal headers are now included in version control, C header is not copied to output anymore; hgignore fix.

---
+++ Terminal/Include/C#/BearLibTerminal.cs
@@ -0,0 +1,327 @@
+﻿/*
+* BearLibTerminal C# wrapper
+* Copyright (C) 2013 Cfyz
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Release date: 2013-12-27
+*/
+
+using System;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace BearLib
+{
+    public static class Terminal
+    {
+        public struct Keys
+        {
+            public const int LeftButton     = 0x01;
+            public const int RightButton    = 0x02;
+            public const int Close          = 0x03; // Same code as VK_CANCEL from winuser.h
+            public const int Back           = 0x08; // Backspace
+            public const int Backspace      = 0x08; // Backspace (alias)
+            public const int Tab            = 0x09; 
+            public const int Return         = 0x0D; // Enter
+            public const int Shift          = 0x10;
+            public const int Control        = 0x11;
+            public const int Pause          = 0x13; // Pause/Break
+            public const int Escape         = 0x1B;
+            public const int Space          = 0x20;
+            public const int Prior          = 0x21; // Page Up
+            public const int Next           = 0x22; // Page Down
+            public const int End            = 0x23;
+            public const int Home           = 0x24;
+            public const int Left           = 0x25; // Left arrow
+            public const int Up             = 0x26; // Up arrow
+            public const int Right          = 0x27; // Right arrow
+            public const int Down           = 0x28; // Down arrow
+            public const int Insert         = 0x2D;
+            public const int Delete         = 0x2E;
+            public const int D0             = 0x30;
+            public const int D1             = 0x31;
+            public const int D2             = 0x32;
+            public const int D3             = 0x33;
+            public const int D4             = 0x34;
+            public const int D5             = 0x35;
+            public const int D6             = 0x36;
+            public const int D7             = 0x37;
+            public const int D8             = 0x38;
+            public const int D9             = 0x39;
+            public const int A              = 0x41;
+            public const int B              = 0x42;
+            public const int C              = 0x43;
+            public const int D              = 0x44;
+            public const int E              = 0x45;
+            public const int F              = 0x46;
+            public const int G              = 0x47;
+            public const int H              = 0x48;
+            public const int I              = 0x49;
+            public const int J              = 0x4A;
+            public const int K              = 0x4B;
+            public const int L              = 0x4C;
+            public const int M              = 0x4D;
+            public const int N              = 0x4E;
+            public const int O              = 0x4F;
+            public const int P              = 0x50;
+            public const int Q              = 0x51;
+            public const int R              = 0x52;
+            public const int S              = 0x53;
+            public const int T              = 0x54;
+            public const int U              = 0x55;
+            public const int V              = 0x56;
+            public const int W              = 0x57;
+            public const int X              = 0x58;
+            public const int Y              = 0x59;
+            public const int Z              = 0x5A;
+            public const int Grave          = 0xC0; //  ` 
+            public const int Minus          = 0xBD; //  -
+            public new const int Equals     = 0xBB; //  =
+            public const int Backslash      = 0xDC; //  \ .
+            public const int LeftBracket    = 0xDB; //  [
+            public const int RightBracket   = 0xDD; //  ]
+            public const int Semicolon      = 0xBA; //  ;
+            public const int Apostrophe     = 0xDE; //  '
+            public const int Comma          = 0xBC; //  ,
+            public const int Period         = 0xBE; //  .
+            public const int Slash          = 0xBF; //  /
+            public const int Numpad0        = 0x60;
+            public const int Numpad1        = 0x61;
+            public const int Numpad2        = 0x62;
+            public const int Numpad3        = 0x63;
+            public const int Numpad4        = 0x64;
+            public const int Numpad5        = 0x65;
+            public const int Numpad6        = 0x66;
+            public const int Numpad7        = 0x67;
+            public const int Numpad8        = 0x68;
+            public const int Numpad9        = 0x69;
+            public const int Multiply       = 0x6A; // '*' on numpad
+            public const int Add            = 0x6B; // '+' on numpad
+            public const int Subtract       = 0x6D; // '-' on numpad
+            public const int Decimal        = 0x6E; // '.' on numpad
+            public const int Divide         = 0x6F; // '/' on numpad
+            public const int F1             = 0x70;
+            public const int F2             = 0x71;
+            public const int F3             = 0x72;
+            public const int F4             = 0x73;
+            public const int F5             = 0x74;
+            public const int F6             = 0x75;
+            public const int F7             = 0x76;
+            public const int F8             = 0x77;
+            public const int F9             = 0x78;
+            public const int F10            = 0x79;
+            public const int F11            = 0x7A;
+            public const int F12            = 0x7B;
+            public const int FlagReleased   = 0x100; // This flag is set if key was released instead of pressed
+        }
+        
+        public struct Events
+        {
+        	public const int MouseMove      = 0xD3; // Mouse movement event
+            public const int MouseScroll    = 0xD4; // Mouse wheel scroll event
+        }
+
+        public struct States
+        {
+            public const int MouseX	        = 0xD0; // Mouse cursor position in characters
+            public const int MouseY         = 0xD1;
+            public const int MousePixelX    = 0xD5; // Mouse cursor position in pixels
+            public const int MousePixelY    = 0xD6;
+            public const int MouseWheel     = 0xD2;
+            public const int CellWidth      = 0xD7; // Character cell size in pixels
+            public const int CellHeight     = 0xD8;
+            public const int Width			= 0xD9; // Screen size in cells
+            public const int Height			= 0xDA;
+            public const int Composition	= 0xC1; // Current composition state
+			public const int Color			= 0xC2; // Current foregroung color
+			public const int BkColor		= 0xC3; // Current background color
+			public const int Layer			= 0xC4; // Current layer
+        }
+        
+        public struct ReadFlags
+        {
+        	public const int Character      = 1;
+        	public const int NoRemove       = 2;
+        }
+
+        public struct CompositionMode
+        {
+            public const int Off            = 0;
+            public const int On             = 1;
+        }
+        
+        public struct InputResult
+        {
+        	public const int None           =  0;
+        	public const int Cancelled      = -1;
+        	public const int CallAgain      = -2;
+        }
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_open", CallingConvention=CallingConvention.Cdecl)]
+        public static extern int Open();
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_close", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Close();
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_refresh", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Refresh();
+
+        [DllImport("BearLibTerminal.dll", CharSet = CharSet.Unicode, EntryPoint = "terminal_set16", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Set(string options);
+
+        public static int Set(string options, params object[] args)
+        {
+        	Dictionary<Bitmap, BitmapData> bitmaps = new Dictionary<Bitmap, BitmapData>();
+        	for (int i=0; i<args.Length; i++)
+        	{
+        		if (args[i] is Bitmap)
+        		{
+        			Bitmap bitmap = args[i] as Bitmap;
+        			BitmapData data = bitmap.LockBits
+					(
+						new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+						ImageLockMode.ReadOnly,
+						PixelFormat.Format32bppArgb
+					);
+					bitmaps[bitmap] = data;
+					args[i] = string.Format("0x{0:X}", (System.UInt64)data.Scan0.ToInt64());
+        		}
+        	}
+        	int rc = Set(string.Format(options, args));
+        	foreach(KeyValuePair<Bitmap, BitmapData> i in bitmaps)
+        	{
+        		i.Key.UnlockBits(i.Value);
+        	}
+            return rc;
+        }
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_clear", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Clear();
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_clear_area", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ClearArea(int x, int y, int w, int h);
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_color", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ColorImpl(int argb);
+
+        public static void Color(Color color)
+        {
+            ColorImpl(color.ToArgb());
+        }
+        
+        public static void Color(string name)
+        {
+        	ColorImpl(ColorFromNameImpl(name));
+        }
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_bkcolor", CallingConvention=CallingConvention.Cdecl)]
+        private static extern void BkColorImpl(int argb);
+
+        public static void BkColor(Color color)
+        {
+            BkColorImpl(color.ToArgb());
+        }
+        
+        public static void BkColor(string name)
+        {
+        	BkColorImpl(ColorFromNameImpl(name));
+        }
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_composition", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Composition(int mode);
+        
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_layer", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Layer(int mode);
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_put", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Put(int x, int y, int code);
+
+        public static void Put(int x, int y, char code)
+        {
+            Put(x, y, (int)code);
+        }
+        
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_put_ext", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void PutExtImpl(int x, int y, int dx, int dy, int code, [MarshalAs(UnmanagedType.LPArray)]System.Int32[] corners);
+        
+        public static void PutExt(int x, int y, int dx, int dy, int code)
+        {
+        	PutExtImpl(x, y, dx, dy, code, null);
+        }
+        
+        public static void PutExt(int x, int y, int dx, int dy, char code)
+        {
+        	PutExt(x, y, dx, dy, (int)code);
+        }
+        
+        public static void PutExt(int x, int y, int dx, int dy, int code, Color[] corners)
+        {
+        	System.Int32[] values = new System.Int32[4];
+        	for (int i=0; i<4; i++) values[i] = corners[i].ToArgb();
+        	PutExtImpl(x, y, dx, dy, code, values);
+        }
+        
+        public static void PutExt(int x, int y, int dx, int dy, char code, Color[] corners)
+        {
+        	PutExt(x, y, dx, dy, (int)code, corners);
+        }
+
+        [DllImport("BearLibTerminal.dll", CharSet = CharSet.Unicode, EntryPoint = "terminal_print16", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Print(int x, int y, string text);
+
+        public static int Print(int x, int y, string text, params object[] args)
+        {
+            return Print(x, y, string.Format(text, args));
+        }
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_has_input", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool HasInput();
+        
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_state", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int State(int code);
+
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_read", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Read();
+        
+        [DllImport("BearLibTerminal.dll", EntryPoint = "terminal_read_ext", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ReadExt(int flags);
+
+        [DllImport("BearLibTerminal.dll", CharSet = CharSet.Unicode, EntryPoint = "terminal_read_str16", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ReadStr(int x, int y, StringBuilder text, int max);
+
+        public static int ReadStr(int x, int y, ref string text, int max)
+        {
+            StringBuilder buffer = new StringBuilder(text, max);
+            int result = ReadStr(x, y, buffer, max);
+            text = buffer.ToString();
+            return result;
+        }
+
+        [DllImport("BearLibTerminal.dll", CharSet = CharSet.Unicode, EntryPoint = "color_from_name16", CallingConvention=CallingConvention.Cdecl)]
+        private static extern int ColorFromNameImpl(string name);
+
+        public static Color ColorFromName(string name)
+        {
+            return System.Drawing.Color.FromArgb(ColorFromNameImpl(name));
+        }
+    }
+}
---
+++ Terminal/Include/Pascal/BearLibTerminal.pas
@@ -0,0 +1,350 @@
+{*
+* BearLibTerminal
+* Copyright (C) 2013 Cfyz
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Release date: 2013-12-27
+*}
+
+{$H+}
+
+unit BeaRLibTerminal;
+
+interface  
+
+uses ctypes;
+
+const
+  // Keyboard scancodes
+  TK_LBUTTON        = $01;
+  TK_RBUTTON        = $02;
+  TK_CLOSE          = $03; // Same code as VK_CANCEL from winuser.h
+  TK_BACK           = $08; // Backspace
+  TK_BACKSPACE      = $08; // Backspace (alias)
+  TK_TAB            = $09;
+  TK_RETURN         = $0D; // Enter
+  TK_SHIFT          = $10;
+  TK_CONTROL        = $11;
+  TK_PAUSE          = $13; // Pause/Break
+  TK_ESCAPE         = $1B;
+  TK_SPACE          = $20;
+  TK_PRIOR          = $21; // Page Up
+  TK_NEXT           = $22; // Page Down
+  TK_END            = $23;
+  TK_HOME           = $24;
+  TK_LEFT           = $25; // Left arrow
+  TK_UP             = $26; // Up arrow
+  TK_RIGHT          = $27; // Right arrow
+  TK_DOWN           = $28; // Down arrow
+  TK_INSERT         = $2D;
+  TK_DELETE         = $2E;
+  TK_0              = $30;
+  TK_1              = $31;
+  TK_2              = $32;
+  TK_3              = $33;
+  TK_4              = $34;
+  TK_5              = $35;
+  TK_6              = $36;
+  TK_7              = $37;
+  TK_8              = $38;
+  TK_9              = $39;
+  TK_A              = $41;
+  TK_B              = $42;
+  TK_C              = $43;
+  TK_D              = $44;
+  TK_E              = $45;
+  TK_F              = $46;
+  TK_G              = $47;
+  TK_H              = $48;
+  TK_I              = $49;
+  TK_J              = $4A;
+  TK_K              = $4B;
+  TK_L              = $4C;
+  TK_M              = $4D;
+  TK_N              = $4E;
+  TK_O              = $4F;
+  TK_P              = $50;
+  TK_Q              = $51;
+  TK_R              = $52;
+  TK_S              = $53;
+  TK_T              = $54;
+  TK_U              = $55;
+  TK_V              = $56;
+  TK_W              = $57;
+  TK_X              = $58;
+  TK_Y              = $59;
+  TK_Z              = $5A;
+  TK_GRAVE          = $C0; //  `
+  TK_MINUS          = $BD; //  -
+  TK_EQUALS         = $BB; //  =
+  TK_BACKSLASH      = $DC; //  \ .
+  TK_LBRACKET       = $DB; //  [
+  TK_RBRACKET       = $DD; //  ]
+  TK_SEMICOLON      = $BA; //  ;
+  TK_APOSTROPHE     = $DE; //  '
+  TK_COMMA          = $BC; //  ,
+  TK_PERIOD         = $BE; //  .
+  TK_SLASH          = $BF; //  /
+  TK_NUMPAD0        = $60;
+  TK_NUMPAD1        = $61;
+  TK_NUMPAD2        = $62;
+  TK_NUMPAD3        = $63;
+  TK_NUMPAD4        = $64;
+  TK_NUMPAD5        = $65;
+  TK_NUMPAD6        = $66;
+  TK_NUMPAD7        = $67;
+  TK_NUMPAD8        = $68;
+  TK_NUMPAD9        = $69;
+  TK_MULTIPLY       = $6A; // '*' on numpad
+  TK_ADD            = $6B; // '+' on numpad
+  TK_SUBTRACT       = $6D; // '-' on numpad
+  TK_DECIMAL        = $6E; // '.' on numpad
+  TK_DIVIDE         = $6F; // '/' on numpad
+  TK_F1             = $70;
+  TK_F2             = $71;
+  TK_F3             = $72;
+  TK_F4             = $73;
+  TK_F5             = $74;
+  TK_F6             = $75;
+  TK_F7             = $76;
+  TK_F8             = $77;
+  TK_F9             = $78;
+  TK_F10            = $79;
+  TK_F11            = $7A;
+  TK_F12            = $7B;
+  
+  // If key was released instead of pressed, it's code will be OR'ed with VK_FLAG_RELEASED constant
+  TK_FLAG_RELEASED  = $100; // This flag is set if key was released instead of pressed
+  
+  // Specific input events
+  TK_MOUSE_MOVE     = $D3; // Mouse movement event
+  TK_MOUSE_SCROLL   = $D4; // Mouse wheel scroll event
+
+  // Virtual codes for various virtual states:
+  TK_MOUSE_X        = $D0; // Mouse cursor position in cells
+  TK_MOUSE_Y        = $D1;
+  TK_MOUSE_PIXEL_X  = $D5; // Mouse cursor position in pixels
+  TK_MOUSE_PIXEL_Y  = $D6;
+  TK_MOUSE_WHEEL    = $D2; // Mouse wheel counter (absolute value)
+  TK_CELL_WIDTH     = $D7; // Character cell size in pixels
+  TK_CELL_HEIGHT    = $D8;
+  TK_WIDTH          = $D9; // Terminal window size in cells
+  TK_HEIGHT         = $DA;
+  TK_COMPOSITION    = $C1; // Current composition state
+  TK_COLOR			= $C2; // Current foregroung color
+  TK_BKCOLOR		= $C3; // Current background color
+  TK_LAYER			= $C4; // Current layer
+
+  // Composition option. If turned on it allows for placing several tiles in one cell.
+  TK_COMPOSITION_OFF = 0;
+  TK_COMPOSITION_ON  = 1;
+  
+  // Input result codes for terminal_read, terminal_read_char and terminal_read_str.
+  TK_INPUT_NONE       =  0;
+  TK_INPUT_CANCELLED  = -1;
+  TK_INPUT_CALL_AGAIN = -2;
+  
+  // Extended reading flags
+  TK_READ_CHAR        = 1; // Read an Unicode character, not a virtual key code
+  TK_READ_NOREMOVE    = 2; // Do not remove the event from input queue
+
+type
+  Color = CUInt32;
+  PColor = ^Color;
+
+// Open
+function terminal_open(): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_open';
+
+// Close
+procedure terminal_close();
+  cdecl; external 'BearLibTerminal' name 'terminal_close';
+
+// Set
+function terminal_set(const Options: PChar): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_set8';
+
+function terminal_set(const Options: String): Integer;
+
+function terminal_set(const Options: PUnicodeChar): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_set16';
+
+function terminal_set(const Options: UnicodeString): Integer;
+
+// Refresh
+procedure terminal_refresh();
+  cdecl; external 'BearLibTerminal' name 'terminal_refresh';
+
+// Clear
+procedure terminal_clear();
+  cdecl; external 'BearLibTerminal' name 'terminal_clear';
+
+// ClearArea
+procedure terminal_clear_area(Left, Top, Width, Height: Integer);
+  cdecl; external 'BearLibTerminal' name 'terminal_clear_area';
+
+// Color
+procedure terminal_color(Color: Color);
+  cdecl; external 'BearLibTerminal' name 'terminal_color';
+  
+procedure terminal_color(Color: String);
+
+procedure terminal_color(Color: UnicodeString);
+
+// BkColor
+procedure terminal_bkcolor(Color: Color);
+  cdecl; external 'BearLibTerminal' name 'terminal_bkcolor';
+
+procedure terminal_bkcolor(Color: String);
+
+procedure terminal_bkcolor(Color: UnicodeString);
+
+// Composition
+procedure terminal_composition(Mode: Integer);
+  cdecl; external 'BearLibTerminal' name 'terminal_composition';
+
+// Layer
+procedure terminal_layer(Mode: Integer);
+  cdecl; external 'BearLibTerminal' name 'terminal_layer';
+
+// Put
+procedure terminal_put(X, Y: Integer; Code: Integer);
+  cdecl; external 'BearLibTerminal' name 'terminal_put';
+
+procedure terminal_put(X, Y: Integer; Code: UnicodeChar);
+
+// PutExt
+procedure terminal_put_ext(X, Y, dX, dY: Integer; Code: Integer; Corners: PColor);
+  cdecl; external 'BearLibTerminal' name 'terminal_put_ext';
+
+procedure terminal_put_ext(X, Y, dX, dY: Integer; Code: UnicodeChar; Corners: PColor);
+
+// Print
+function terminal_print(X, Y: Integer; const s: PChar): Cardinal;
+  cdecl; external 'BearLibTerminal' name 'terminal_print8';
+
+function terminal_print(X, Y: Integer; const S: string): Cardinal;
+
+function terminal_print(X, Y: Integer; const s: PUnicodeChar): Cardinal;
+  cdecl; external 'BearLibTerminal' name 'terminal_print16';
+
+function terminal_print(X, Y: Integer; const S: UnicodeString): Cardinal;
+
+// HasInput
+function terminal_has_input(): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_has_input';
+
+// State
+function terminal_state(Code: Integer): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_state';
+
+// Read
+function terminal_read(): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_read';
+
+// ReadExt
+function terminal_read_ext(Flags: Integer): Integer;
+  cdecl; external 'BearLibTerminal' name 'terminal_read_ext';
+
+// ColorFromName
+function color_from_name(const Name: PChar): Color;
+  cdecl; external 'BearLibTerminal' name 'color_from_name8';
+
+function color_from_name(const Name: String): Color;
+
+function color_from_name(const Name: PUnicodeChar): Color;
+  cdecl; external 'BearLibTerminal' name 'color_from_name16';
+
+function color_from_name(const Name: UnicodeString): Color;
+
+// ColorFromARGB
+function color_from_argb(a, r, g, b: Integer): Color;
+
+implementation
+
+function terminal_set(const Options: String): Integer;
+begin
+    terminal_set := terminal_set(PChar(Options));
+end;
+
+function terminal_set(const Options: UnicodeString): Integer;
+begin
+    terminal_set := terminal_set(PUnicodeChar(Options));
+end;
+
+procedure terminal_color(Color: String);
+begin
+	terminal_color(color_from_name(Color));
+end;
+
+procedure terminal_color(Color: UnicodeString);
+begin
+	terminal_color(color_from_name(Color));
+end;
+
+procedure terminal_bkcolor(Color: String);
+begin
+	terminal_bkcolor(color_from_name(Color));
+end;
+
+procedure terminal_bkcolor(Color: UnicodeString);
+begin
+	terminal_bkcolor(color_from_name(Color));
+end;
+
+procedure terminal_put(X, Y: Integer; Code: UnicodeChar);
+begin
+    terminal_put(X, Y, ord(Code));
+end;
+
+procedure terminal_put_ext(X, Y, dX, dY: Integer; Code: UnicodeChar; Corners: PColor);
+begin
+	terminal_put_ext(X, Y, dX, dY, ord(Code), Corners);
+end;
+
+function terminal_print(X, Y: Integer; const S: string): Cardinal;
+begin
+    terminal_print := terminal_print(X, Y, PChar(S));
+end;
+
+function terminal_print(X, Y: Integer; const S: UnicodeString): Cardinal;
+begin
+     terminal_print := terminal_print(X, Y, PUnicodeChar(S));
+end;
+
+function color_from_name(const Name: string): Color;
+begin
+    color_from_name := color_from_name(PChar(Name));
+end;
+
+function color_from_name(const Name: UnicodeString): Color;
+begin
+    color_from_name := color_from_name(PUnicodeChar(Name));
+end;
+
+function color_from_argb(a, r, g, b: Integer): Color;
+begin
+    if a > 255 then a := 255;
+    if r > 255 then r := 255;
+    if g > 255 then g := 255;
+    if b > 255 then b := 255;
+    color_from_argb := (a shl 24) or (r shl 16) or (g shl 8) or b;
+end;
+
+end.
