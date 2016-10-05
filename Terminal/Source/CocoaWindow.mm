a0b03a11bcb4727497fcdb332ab778c5fbbf5b4d
Alexander Malinin <cfyzium@gmail.com>
2016-10-04 18:27:07 -0700

    Centering the window in OS X. Closes issue #15

--- Terminal/Source/CocoaWindow.mm
+++ Terminal/Source/CocoaWindow.mm
@@ -180,6 +180,7 @@ namespace BearLibTerminal
         Size m_increment;
         Size m_minimum_size;
         bool m_cursor_visible;
+        bool m_has_been_shown;
         id m_window;
         id m_view;
     };
@@ -188,6 +189,7 @@ namespace BearLibTerminal
         m_handler(handler),
         m_resizeable(false),
         m_cursor_visible(true),
+        m_has_been_shown(false),
         m_window(nil),
         m_view(nil)
     { }
@@ -446,9 +448,11 @@ namespace BearLibTerminal
     
     void CocoaWindow::SetClientSize(const Size& size)
     {
-        NSRect prev = [m_impl->m_window frame];
-        NSRect next = NSMakeRect(prev.origin.x, prev.origin.y, size.width, size.height);
-        NSRect frame = [m_impl->m_window frameRectForContentRect:next];
+        NSRect frame = [m_impl->m_window frame];
+        NSRect next = NSMakeRect(0, 0, size.width, size.height);
+        next = [m_impl->m_window frameRectForContentRect:next];
+        frame.origin.y += (frame.size.height - next.size.height);
+        frame.size = next.size;
         [m_impl->m_window setFrame:frame display:YES];
         m_client_size = size;
     }
@@ -456,6 +460,11 @@ namespace BearLibTerminal
     void CocoaWindow::Show()
     {
         [NSApp unhide:nil];
+        if (!m_impl->m_has_been_shown)
+        {
+            [m_impl->m_window center];
+            m_impl->m_has_been_shown = true;
+        }
         [m_impl->m_window makeKeyAndOrderFront:nil];
         [m_impl->m_window makeMainWindow];
     }
